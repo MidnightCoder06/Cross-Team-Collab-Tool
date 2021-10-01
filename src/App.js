98f29284296c6cc9ee6651aaf7423414a0c6441c
Jean Emile Leconte II <jeanemileleconteii@Jeans-MacBook-Air.local>
2021-09-30 21:50:14 -0500

    added routes to other components and click ability to them from navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import PurposeStatement from './components/PurposeStatement';
+import TaskCreationForm from './components/TaskCreationForm';
+import BugReportForm from './components/BugReportForm';
 
 const App = ()  => (
   <Router>
     <Route exact path="/" component={LandingPage} />
+    <Route path="/purpose-statement" component={PurposeStatement} />
+    <Route path="/task-creation-form" component={TaskCreationForm} />
+    <Route path="/bug-report-form" component={BugReportForm} />
   </Router>
 );
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,38 @@
 import '../styles/Navbar.css'
+import { useHistory } from 'react-router-dom';
+
+/* create dropdown menu next
+    https://www.youtube.com/watch?v=IF6k0uZuypA&t=110s
+    https://www.youtube.com/watch?v=mN3P_rv8ad4
+    https://www.youtube.com/watch?v=t8JK5bVoVBw
+
+*/
 
 const Navbar = () => {
+    const history = useHistory();
+
+    const handlePurposeStatementClick = () => {
+        history.push('./purpose-statement');
+    }
+
+    const handleCreateTaskClick = () => {
+        history.push('/task-creation-form');
+    }
+
+    const handleFileBugClick = () => {
+        history.push('/bug-report-form');
+    }
+
     return (
         <div id="parent">
-            <div class="child"> Purpose </div>
-            <div class="child"> Sort </div>
-            <div class="child"> Create a task </div>
-            <a href="https://github.com/MidnightCoder06/Cross-Team-Collab-Tool"><div class="child"> Contribute </div></a>
-            <div class="child"> File a bug </div>
+            <button className="child" id="button" onClick={handlePurposeStatementClick}> Purpose </button>
+            <select name="Sort" className="child"> 
+                <option value="Priority"> Priority </option>
+                <option value="Date Posted"> Date Posted </option>
+            </select>
+            <button className="child" id="button" onClick={handleCreateTaskClick}> Create a task </button>
+            <a href="https://github.com/MidnightCoder06/Cross-Team-Collab-Tool"><div className="child"> Contribute </div></a>
+            <button className="child" id="button" onClick={handleFileBugClick}> File a bug </button>
         </div>
     );
 }
